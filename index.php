67019f396998a94f1bce055a114a1890ea182512
franco30 <francolatosa30@yahoo.com>
2019-01-26 17:35:28 +0800

    doing initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>BRUHXIX</title>
+    </head>
+    <body>
+    
+        <p>This is partial php file</p>
+        <?php echo "Hello World";?>
+    
+    </body>
+</html>
